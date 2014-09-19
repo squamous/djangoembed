29d18fdb6605b3ac38a031b80734e7253e7a83c3
squamous <paul.mackin@gmail.com>
2014-09-20 05:31:50 +1000

    Update sites.py

--- oembed/sites.py
+++ oembed/sites.py
@@ -4,7 +4,10 @@ import re
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import signals
-from django.utils import simplejson as json
+try:
+    from django.utils import simplejson as json
+except ImportError:
+    import simplejson as json
 
 from oembed.constants import DEFAULT_OEMBED_TTL, MIN_OEMBED_TTL, RESOURCE_TYPES
 from oembed.exceptions import AlreadyRegistered, NotRegistered, OEmbedMissingEndpoint, OEmbedException
